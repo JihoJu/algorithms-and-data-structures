de770dbcdfc9fae13f561c6249ab44a53c7b64a3
주지호 <75026409+JihoJu@users.noreply.github.com>
2022-08-11 19:48:37 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,21 +1,18 @@
 def solution(N, stages):
-    f_stages = [0 for _ in range(N + 1)]  # 스테이지마다 실패 유저 수 표시 (스테이지 - 1 = index)
-    f_rates = list()  # (실패율, stage)
+    fails = [0 for _ in range(N + 1)]  # 스테이지마다 실패 유저 수 표시 (스테이지 - 1 = index)
+    fail_rates = dict()
     
     for stage in stages:
-        f_stages[stage - 1] += 1
+        fails[stage - 1] += 1
     
     total = len(stages) # 총 유저 수
-    for i, n in enumerate(f_stages):
-        if total == 0:
-            f_rates.append((0.0, i + 1))
+    for stage in range(N):
+        if total != 0:
+            fail_rates[stage + 1] = float(fails[stage]) / total
+            total -= fails[stage]
         else:
-            f_rates.append((float(n) / total, i + 1)) 
-        total -= f_stages[i]
-        if i == (N - 1):
-            break
+            fail_rates[stage + 1] = 0.0
+
+    answer = sorted(fail_rates.keys(), key=lambda x: fail_rates[x], reverse=True)
     
-    f_rates = sorted(f_rates, key=lambda f_rate: f_rate[0], reverse=True)
-    answer = [stage[1] for stage in f_rates]
-        
     return answer
