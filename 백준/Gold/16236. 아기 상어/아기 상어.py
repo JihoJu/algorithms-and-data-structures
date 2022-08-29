e9e615e7da159474a6a4476b3cf27e41d8de91d7
주지호 <75026409+JihoJu@users.noreply.github.com>
2022-08-29 17:20:08 +0900

    [Gold III] Title: 아기 상어, Time: 180 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,81 +1,76 @@
 import sys
 from collections import deque
-import functools
 
 
 input = sys.stdin.readline
 
 
-def compare(a, b):
-    if a[0] == b[0]:
-        return a[1] - b[1]
-    return a[0] - b[0]
-
-
 def dfs():
-    global shark_x, shark_y, N
+    global N, shark_x, shark_y
+    min_move = float("inf")
 
-    min_move = float("inf")  # 상어 초기 이동 거리 무한대 가정
-    eq_distance = []  # 가장 가까운 먹을 수 있는 물고기 중 거리가 같은 물고기 위치
-    visited = [[0 for _ in range(N)] for _ in range(N)]
-    visited[shark_x][shark_y] = 1
+    visited = [[0 for _ in range(N)] for _ in range(N)]  # 방문 정보
+    eq_dis_fish = []  # 같은 위치의 잡아먹을 수 있는 물고기 위치 -> dfs 로 최소거리를 구한 후 같은 거리에 있는 물고기 insert
+
     dq = deque()
     dq.append((shark_x, shark_y, 0))
+    visited[shark_x][shark_y] = 1
 
     while len(dq) != 0:
-        px, py, move_cnt = dq.popleft()
+        px, py, move = dq.popleft()
         for i in range(4):
             mx = px + dx[i]
             my = py + dy[i]
 
             if 0 <= mx < N and 0 <= my < N and not visited[mx][my]:
-                if MAP[mx][my] != 0 and MAP[mx][my] < shark_size[0]:
-                    # 현재 먹을 수 있는 물고기까지 움직인 거리 > 이전에 먹은 물고기까지의 거리
-                    if move_cnt + 1 > min_move:
-                        continue
-                    min_move = min(move_cnt + 1, min_move)
-                    eq_distance.append((mx, my))
-                    visited[mx][my] = 1
-                elif MAP[mx][my] == 0 or MAP[mx][my] == shark_size[0]:
-                    dq.append((mx, my, move_cnt + 1))
+                # 상어가 물고기를 잡아 먹을 수 있는 case
+                if M[mx][my] != 0 and M[mx][my] < shark_info[0]:
+                    # dfs 로 정해진 최소 거리보다 작거나 같은 경우 => == 등호도 상관 x
+                    if (move + 1) <= min_move:
+                        min_move = min(min_move, move + 1)
+                        eq_dis_fish.append((mx, my))
+                        visited[mx][my] = 1
+                # 상어가 지나갈 수 있는 case
+                elif M[mx][my] == 0 or M[mx][my] == shark_info[0]:
+                    dq.append((mx, my, move + 1))
                     visited[mx][my] = 1
 
-    # 먹을 먹이가 아무것도 없는 경우
+    # 잡아먹을 수 있는 물고기가 없는 경우
     if min_move == float("inf"):
         return -1
 
-    if len(eq_distance) != 1:  # 거리가 같은 물고기가 1마리보다 많은 경우
-        eq_distance = sorted(eq_distance, key=functools.cmp_to_key(compare))
-
-    MAP[shark_x][shark_y] = 0  # 기존 상어 위치 0 으로
-    shark_x, shark_y = eq_distance[0]
-    MAP[shark_x][shark_y] = 9  # 이동한 상어 위치 9
-    shark_size[1] += 1  # 물고기 먹은 횟수
-    if shark_size[0] == shark_size[1]:  # 상어 크기와 먹은 물고기 수가 같을 경우
-        shark_size[0] += 1  # 상어 사이즈 + 1
-        shark_size[1] = 0  # 먹은 물고기 수 초기화
+    # 가장 위에 있는 물고기, 가장 왼쪽에 있는 물고기
+    shark_x, shark_y = sorted(eq_dis_fish, key=lambda x: (x[0], x[1]))[0]
+    M[shark_x][shark_y] = 0
+    # 상어 정보 업데이트
+    shark_info[1] += 1
+    if shark_info[0] == shark_info[1]:
+        shark_info[0] += 1
+        shark_info[1] = 0
 
     return min_move
 
 
 dx = [0, 0, -1, 1]  # 좌우상하
-dy = [-1, 1, 0, 0]
+dy = [-1, 1, 0, 0]  # 좌우상하
 
 N = int(input())
-MAP = [list(map(int, input().strip().split())) for _ in range(N)]
+M = [list(map(int, input().strip().split())) for _ in range(N)]
 
-fish_pos = deque()  # 모든 물고기 위치 (사이즈, x, y)
-total_cnt = 0
-shark_size = [2, 0]  # [0]: shark size, [1]: 잡아먹은 물고기
-
+# 상어 위치 찾기
 for i in range(N):
     for j in range(N):
-        if MAP[i][j] == 9:
+        if M[i][j] == 9:
             shark_x, shark_y = i, j
+            M[i][j] = 0
+            break
+
+shark_info = [2, 0]  # [size, eat_cnt]
+total_cnt = 0  # 총 이동거리
 
 while True:
     move_cnt = dfs()
-    if move_cnt == -1:  # 먹을 수 있는 먹이가 아무것도 없는 경우
+    if move_cnt == -1:
         break
     total_cnt += move_cnt
 
