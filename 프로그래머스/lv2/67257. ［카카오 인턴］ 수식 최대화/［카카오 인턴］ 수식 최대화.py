58e3a1e56646364d22bef741c818fab6d8e5a125
주지호 <75026409+JihoJu@users.noreply.github.com>
2022-09-05 19:10:03 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 0.17 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.py
@@ -1,9 +1,6 @@
 import re
 from collections import deque
-
-priority = [0, 0, 0]    # 우선순위 경우의 수 표현
-op = ['+', '-', '*']    # 연산자
-max_value = float('-inf')
+from itertools import permutations
 
 def calculate(a, b, op):
     if op == '+':
@@ -12,33 +9,54 @@ def calculate(a, b, op):
         return a - b
     return a * b
 
-def dfs(exp):
-    global max_value
-    # 주어진 수식이 모두 계산된 경우
-    if len(exp) == 1:
-        max_value = max(abs(exp[0]), max_value) # 최대값 구하기
-        return
+# priority = [0, 0, 0]    # 우선순위 경우의 수 표현
+# op = ['+', '-', '*']    # 연산자
+# max_value = float('-inf')
+
+# def dfs(exp):
+#     global max_value
+#     # 주어진 수식이 모두 계산된 경우
+#     if len(exp) == 1:
+#         max_value = max(abs(exp[0]), max_value) # 최대값 구하기
+#         return
     
-    for i in range(3):
-        # 이미 우선순위가 정해진 경우
-        if priority[i]:
-            continue
-        priority[i] = 1
-        copy_exp = exp.copy()
-        cal_exp = deque()   # 계산 결과
-        while len(copy_exp) != 0:
-            e = copy_exp.popleft()
-            if e == op[i]:
-                cal_exp.append(calculate(int(cal_exp.pop()), int(copy_exp.popleft()), op[i]))
-            else:
-                cal_exp.append(e)
-        dfs(cal_exp)
-        priority[i] = 0
+#     for i in range(3):
+#         # 이미 우선순위가 정해진 경우
+#         if priority[i]:
+#             continue
+#         priority[i] = 1
+#         copy_exp = exp.copy()
+#         cal_exp = deque()   # 계산 결과
+#         while len(copy_exp) != 0:
+#             e = copy_exp.popleft()
+#             if e == op[i]:
+#                 cal_exp.append(calculate(int(cal_exp.pop()), int(copy_exp.popleft()), op[i]))
+#             else:
+#                 cal_exp.append(e)
+#         dfs(cal_exp)
+#         priority[i] = 0
         
+# def solution(expression):
+#     exp = deque(re.findall(r'\d+|[^\d]', expression))
+    
+#     dfs(exp)
+    
+#     return max_value
+
 
 def solution(expression):
-    exp = deque(re.findall(r'\d+|[^\d]', expression))
+    operations = [x for x in ['+', '-', '*'] if x in expression]
+    op_case = [list(y) for y in permutations(operations)]   # operation 우선순위 경우의 수
+    exp = re.findall(r'\d+|[^\d]', expression)
     
-    dfs(exp)
-    
-    return max_value
+    calculated = []
+    for ops in op_case:
+        _exp = exp.copy()
+        for op in ops:
+            while op in _exp:
+                tmp = _exp.index(op)
+                _exp[tmp-1] = calculate(int(_exp[tmp-1]), int(_exp[tmp+1]), _exp[tmp])
+                _exp = _exp[:tmp] + _exp[tmp+2:]
+        calculated.append(abs(_exp[0]))
+        
+    return max(calculated)
