47675e9e63f45e94f634d121331316147c22ba13
주지호 <75026409+JihoJu@users.noreply.github.com>
2022-09-15 22:39:45 +0900

    [level 2] Title: [1차] 캐시, Time: 0.92 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,20 +1,18 @@
+from collections import deque
+
 def solution(cacheSize, cities):
     time = 0    # cnt: cache 에 들어 있는 데이터 수
-    cache = dict()
+    cache = deque(maxlen=cacheSize)
+    
     for city in cities:
         city = city.lower()
         # cache 내 city 정보가 존재
         if city in cache:
-            del cache[city]
+            cache.remove(city)
+            cache.append(city)
             time += 1
-        # cache 가 꽉 차있는 경우
-        elif cacheSize != 0 and len(cache) >= cacheSize:
-            del cache[list(cache.keys())[0]]
-            time += 5
         else:
+            cache.append(city)
             time += 5
-        
-        if len(cache) < cacheSize:
-            cache[city] = 0
             
     return time
