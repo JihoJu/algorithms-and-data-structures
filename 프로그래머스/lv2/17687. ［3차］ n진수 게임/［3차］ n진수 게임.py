6739ee840e5f71aa08666da332d1e3a6d59d6cd2
주지호 <75026409+JihoJu@users.noreply.github.com>
2022-09-19 20:42:39 +0900

    [level 2] Title: [3차] n진수 게임, Time: 5.75 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,23 +1,23 @@
-mapping = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+DIGITS = '0123456789ABCDEF'
 
 def transform(n, num):
     if num == 1:
         return '01'
+    
     res = ""
     while num > 0:
-        res += mapping[num % n]
+        res += DIGITS[num % n]
         num //= n
     
     return res[::-1]
 
 def solution(n, t, m, p):
     answer = ""
-    current_num = 1
-    cnt = 0
-    order = 0
+    num = 1
+    cnt, order = 0, 0
     
     while True:
-        t_num = transform(n, current_num)
+        t_num = transform(n, num)
         
         for tn in t_num:
             if order == (p-1):
@@ -26,9 +26,6 @@ def solution(n, t, m, p):
             order = (order + 1) % m
             if cnt >= t:
                 return answer
-        current_num += 1
+        num += 1
     
-    return 1
-
-
-
+    return answer
