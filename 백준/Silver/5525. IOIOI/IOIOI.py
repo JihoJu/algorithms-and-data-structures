7d29c49a8a4bda2eb5197a2c2a880bcf7104f9c2
주지호 <75026409+JihoJu@users.noreply.github.com>
2022-10-10 23:55:35 +0900

    [Silver I] Title: IOIOI, Time: 768 ms, Memory: 59380 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,60 @@
+# IOIOI
+from imp import SEARCH_ERROR
+import math
+from re import A
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+S = input().strip()
+
+
+def get_partial_match(search):
+    m = len(search)
+    ret = [0 for i in range(m)]
+
+    begin = 1
+    match = 0
+
+    while (begin + match) < m:
+
+        if search[begin + match] == search[match]:
+            match += 1
+            ret[begin + match - 1] = match
+
+        else:
+            if match == 0:
+                begin += 1
+            else:
+                begin += match - pi[match - 1]
+                match = pi[match - 1]
+
+    return ret
+
+
+item = ["I" for _ in range(N + 1)]
+p = "O".join(item)
+
+pi = get_partial_match(p)
+
+psize = 2 * N + 1
+begin = 0
+match = 0
+cnt = 0
+
+while begin <= M - psize:
+    if (match < psize) and S[begin + match] == p[match]:
+        match += 1
+
+        if match == psize:
+            cnt += 1
+    else:
+        if match == 0:
+            begin += 1
+        else:
+            begin += match - pi[match - 1]
+            match = pi[match - 1]
+
+print(cnt)
